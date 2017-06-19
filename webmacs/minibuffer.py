aafa45468d9156a6ea03be486c5f67c912263af6
Julien Pagès <j.parkouss@gmail.com>
2017-06-19 11:09:32 +0200

    Implement a custom minibuffer input completer
    To be able to use fuzzy matching, displaying of multiple columns and to show the
    popup on top of the line edit.

--- webmacs/minibuffer.py
+++ webmacs/minibuffer.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QLabel, \
-    QCompleter
-from PyQt5.QtCore import QObject, pyqtSignal as Signal, pyqtSlot as Slot
+    QTableView, QHeaderView, QApplication
+from PyQt5.QtCore import QObject, pyqtSignal as Signal, pyqtSlot as Slot, \
+    QPoint, QEvent, QSortFilterProxyModel, QRegExp, Qt
 
 from .keyboardhandler import KeyboardHandler, is_keypress
 from .keymap import Keymap
@@ -26,8 +27,8 @@ class Prompt(QObject):
         buffer_input = minibuffer.line_edit
         buffer_input.show()
         buffer_input.setFocus()
-        buffer_input.completer().setModel(self.completer_model())
-        buffer_input.edition_finished.connect(self._on_edition_finished)
+        buffer_input.set_completer_model(self.completer_model())
+        buffer_input.returnPressed.connect(self._on_edition_finished)
 
     def close(self):
         minibuffer = self.minibuffer
@@ -35,8 +36,8 @@ class Prompt(QObject):
         buffer_input = minibuffer.line_edit
         buffer_input.hide()
         buffer_input.setText("")
-        c_model = buffer_input.completer().model()
-        buffer_input.completer().setModel(None)
+        c_model = buffer_input.completer_model()
+        buffer_input.set_completer_model(None)
         if c_model:
             c_model.deleteLater()
         self.closed.emit()
@@ -49,11 +50,56 @@ class Prompt(QObject):
         self.close()
 
 
+class Popup(QTableView):
+    def __init__(self):
+        QTableView.__init__(self)
+        self.setParent(None, Qt.Popup)
+        self.setFocusPolicy(Qt.NoFocus)
+        self.horizontalHeader().hide()
+        self.verticalHeader().hide()
+        self.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+        self.verticalHeader().setDefaultSectionSize(24)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setEditTriggers(QTableView.NoEditTriggers)
+        self.setSelectionBehavior(QTableView.SelectRows)
+        self.setSelectionMode(QTableView.SingleSelection)
+        self.setShowGrid(False)
+        self._max_visible_items = 10
+
+    def popup(self, widget):
+        h = (24) * min(self._max_visible_items, self.model().rowCount()) + 3
+
+        w = widget.width()
+        pos = widget.mapToGlobal(QPoint(0, -(h + 1)))
+
+        self.setGeometry(pos.x(), pos.y(), w, h)
+
+        cols = self.model().columnCount()
+        col_width = w / cols
+        for i in range(cols):
+            self.setColumnWidth(i, col_width)
+
+        if not self.isVisible():
+            self.show()
+
+
 class MinibufferInput(QLineEdit):
-    edition_finished = Signal()
+    FuzzyFilter = 0
+    SimpleFilter = 1
 
     def __init__(self, parent=None):
         QLineEdit.__init__(self, parent)
+        self._completer_model = None
+        self._popup = Popup()
+        self.textEdited.connect(self._show_completions)
+        self._popup.installEventFilter(self)
+        self.installEventFilter(self)
+        self._eat_focusout = False
+        self._proxy_model = QSortFilterProxyModel(self)
+        self._proxy_model.setFilterKeyColumn(-1)
+        self._popup.setModel(self._proxy_model)
+        self._popup.activated.connect(self._on_completion_activated)
+        self._filter_type = self.SimpleFilter
         self.keyboard_handler = KeyboardHandler([KEYMAP])
 
     def event(self, event):
@@ -61,30 +107,102 @@ class MinibufferInput(QLineEdit):
             return True
         return QLineEdit.event(self, event)
 
+    def eventFilter(self, obj, event):
+        etype = event.type()
+        if etype == QEvent.FocusOut and obj == self and self._eat_focusout \
+           and self._popup.isVisible():
+            # keep the focus on the line edit
+            return True
+        elif etype == QEvent.MouseButtonPress:
+            # if we've clicked in the widget (or its descendant), let it handle
+            # the click
+            pos = obj.mapToGlobal(event.pos())
+            target = QApplication.widgetAt(pos)
+            if target and (self.isAncestorOf(target) or target == self):
+                if not self._popup.underMouse():
+                    self._popup.hide()
+                target.event(event)
+                return True
 
-class Completer(QCompleter):
-    def highlight_next_completion(self, forward=True):
-        if not self.popup().isVisible():
+            if not self._popup.underMouse():
+                self._popup.hide()
+                return True
+        elif etype in (QEvent.KeyPress, QEvent.KeyRelease):
+            # send event to the line edit
+            self._eat_focusout = True
+            self.event(event)
+            self._eat_focusout = False
+            return True
+
+        return QLineEdit.eventFilter(self, obj, event)
+
+    def set_completer_model(self, completer_model):
+        self._proxy_model.setSourceModel(completer_model)
+
+    def completer_model(self):
+        return self._proxy_model.sourceModel()
+
+    def set_filter_type(self, type):
+        self._filter_type = type
+        if self._popup.isVisible():
+            self._show_completions(self.text)
+
+    def _show_completions(self, txt, force=False):
+        if self._filter_type == self.SimpleFilter:
+            pattern = "^" + QRegExp.escape(txt)
+        else:
+            pattern = ".*".join(QRegExp.escape(t) for t in txt.split())
+
+        self._proxy_model.setFilterRegExp(QRegExp(pattern))
+        if self._proxy_model.rowCount() == 0:
+            self._popup.hide()
+        elif not txt and not force:
+            self._popup.hide()
+        else:
+            self._popup.popup(self)
+
+    def show_completions(self):
+        self._show_completions(self.text(), True)
+
+    def _on_completion_activated(self, index):
+        self._popup.hide()
+        model = index.model()
+        if index.column() != 0:
+            index = model.index(index.row(), 0)
+
+        self.setText(model.data(index))
+
+    def popup(self):
+        return self._popup
+
+    def complete(self):
+        if not self._popup.isVisible():
             return
 
-        row = self.currentRow()
+        index = self._popup.selectionModel().currentIndex()
+        if index.isValid():
+            self._on_completion_activated(index)
 
-        # weird case, the first highlight on forward go to the second item
-        # without this fix
-        if row == 0 and forward:
-            sel = self.popup().selectionModel()
-            if not sel or not sel.hasSelection():
-                row = -1
+    def select_next_completion(self, forward=True):
+        model = self._proxy_model
+        entries = model.rowCount()
+        if entries == 0:
+            return
 
-        count = self.completionModel().rowCount()
-        pos = row + (1 if forward else -1)
+        selection = self._popup.selectionModel().currentIndex()
+        if not selection.isValid():
+            row = 0 if forward else (entries - 1)
+        else:
+            if forward:
+                row = row + 1
+                if row >= entries:
+                    row = 0
+            else:
+                row = row - 1
+                if row < 0:
+                    row = (entries - 1)
 
-        if pos < 0:
-            pos = count - 1
-        elif pos >= count:
-            pos = 0
-        self.setCurrentRow(pos)
-        self.popup().setCurrentIndex(self.currentIndex())
+        self._popup.selectRow(row)
 
 
 class Minibuffer(QWidget):
@@ -100,9 +218,6 @@ class Minibuffer(QWidget):
         self.line_edit = MinibufferInput(self)
         layout.addWidget(self.line_edit)
 
-        self.completer = Completer(self)
-        self.completer.setMaxVisibleItems(10)
-        self.line_edit.setCompleter(self.completer)
         self.line_edit.hide()
         self._prompt = None
 
@@ -130,34 +245,35 @@ def current_minibuffer():
 
 @KEYMAP.define_key("Tab")
 def complete():
-    completer = current_minibuffer().completer
+    input = current_minibuffer().line_edit
 
-    if not completer.popup().isVisible():
-        completer.complete()
+    if not input.popup().isVisible():
+        input.show_completions()
     else:
-        completer.highlight_next_completion()
+        input.select_next_completion()
 
 
 @KEYMAP.define_key("C-n")
 def next_completion():
-    current_minibuffer().completer.highlight_next_completion()
+    current_minibuffer().line_edit.select_next_completion()
 
 
 @KEYMAP.define_key("C-p")
 def previous_completion():
-    current_minibuffer().completer.highlight_next_completion(False)
+    current_minibuffer().line_edit.select_next_completion(False)
 
 
 @KEYMAP.define_key("Return")
 def edition_finished():
-    current_minibuffer().line_edit.edition_finished.emit()
+    current_minibuffer().line_edit.complete()
+    current_minibuffer().line_edit.returnPressed.emit()
 
 
 @KEYMAP.define_key("C-g")
 def cancel():
     minibuffer = current_minibuffer()
-    completer = minibuffer.completer
-    if completer.popup().isVisible():
-        completer.popup().close()
+    input = minibuffer.line_edit
+    if input.popup().isVisible():
+        input.popup().hide()
     else:
         minibuffer.close_prompt()
