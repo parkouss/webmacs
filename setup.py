b15283de11a2f497eda1cac9000d7c9b3329b5bd
Julien Pagès <j.parkouss@gmail.com>
2017-06-30 11:13:36 +0200

    improve detection of the need to download adblock rules

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ Work in progress.
     packages=find_packages(),
     # should be a dependency, but it fail with strange errors on arch;
     # install_requires=["PyQt5"],
+    install_requires=["dateparser"],
     entry_points={"console_scripts": ["webmacs = webmacs.main:main"]},
     python_requires=">=3.3",
     ext_modules=[adblocker])
--- webmacs/adblock.py
+++ webmacs/adblock.py
@@ -1,9 +1,12 @@
 import os
-import time
 import logging
+import time
+
+from datetime import datetime, timezone
+import dateparser
 
 from _adblock import AdBlock
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import urllib.request
 
 
@@ -30,21 +33,46 @@ class Adblocker(object):
         headers = {'User-Agent': "Magic Browser"}
         req = urllib.request.Request(url, None, headers)
         with urllib.request.urlopen(req, timeout=5) as conn:
+            if os.path.isfile(path):
+                # check if the file on the server is newer than what we have
+                file_time = datetime.fromtimestamp(os.path.getmtime(path),
+                                                   timezone.utc)
+                try:
+                    last_modified = dateparser.parse(
+                        conn.info()["last-modified"],
+                        languages=["en"])
+                except:
+                    logging.exception(
+                        "Unable to parse the last-modified header for %s",
+                        url)
+                else:
+                    print(last_modified, file_time)
+                    if last_modified < file_time:
+                        logging.info("no need to download adblock rule: %s",
+                                     url)
+                        # touch on the file
+                        os.utime(path, None)
+                        return False
+            logging.info("downloading adblock rule: %s", url)
             with open(path, "w") as f:
                 data = conn.read()
                 f.write(data.decode("utf-8"))
+            return True
 
     def _fetch_urls(self):
         modified = False
+        # do not try to download if files are less than a day old
         to_download = [(url, path) for url, path in self._urls.items()
                        if not os.path.isfile(path)
                        or os.path.getmtime(path) > (time.time() + 3600)]
         if to_download:
             with ThreadPoolExecutor(max_workers=5) as executor:
-                for url, path in to_download:
-                    executor.submit(self._download_file, url, path)
-                    logging.info("downloading adblock rule: %s", url)
-            modified = True
+                futures = [executor.submit(self._download_file, url, path)
+                           for url, path in to_download]
+
+                for future in as_completed(futures):
+                    if future.result():
+                        modified = True
 
         return modified
 
