404e96a6e9037eb4c65f23d11a883a517831421b
Julien Pagès <julien.pages@easymile.com>
2017-12-18 14:37:51 +0100

    add LICENCE information

--- webmacs/webbuffer.py
+++ webmacs/webbuffer.py
@@ -1,3 +1,18 @@
+# This file is part of webmacs.
+#
+# webmacs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# webmacs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with webmacs.  If not, see <http://www.gnu.org/licenses/>.
+
 import logging
 
 from PyQt5.QtCore import QUrl, pyqtSlot as Slot, QEventLoop
