9e39258f32d0f028accfcbd52932ccbdd1504b50
Julien Pagès <julien.pages@easymile.com>
2018-04-19 12:01:33 +0200

    List commands and variables in webmacs:// scheme handler

--- webmacs/scheme_handlers/webmacs/__init__.py
+++ webmacs/scheme_handlers/webmacs/__init__.py
@@ -14,24 +14,38 @@
 # along with webmacs.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import re
 from PyQt5.QtCore import QBuffer, QFile
 from PyQt5.QtWebEngineCore import QWebEngineUrlSchemeHandler
 from jinja2 import Environment, PackageLoader
-from ... import version
+from ... import version, COMMANDS
+from ...variables import VARIABLES
 
 
-PAGES = {}
+PAGES = []
+_REQUESTS_HANDLER = []
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def register_page(meth):
-    PAGES[meth.__name__] = meth
-    return meth
+def register_page(match_url=None, visible=True):
+    def wrapper(meth):
+        if visible:
+            PAGES.append(meth.__name__)
+        if match_url is None:
+            match = re.compile(r"^(%s)$" % re.escape(meth.__name__))
+        elif isinstance(match_url, str):
+            match = re.compile(match_url)
+        else:
+            match = match_url
+
+        _REQUESTS_HANDLER.append((match, meth))
+
+        return meth
+    return wrapper
 
 
 class WebmacsSchemeHandler(QWebEngineUrlSchemeHandler):
     scheme = b"webmacs"
-    pages = {}
 
     def __init__(self, parent=None):
         QWebEngineUrlSchemeHandler.__init__(self, parent)
@@ -40,39 +54,48 @@ class WebmacsSchemeHandler(QWebEngineUrlSchemeHandler):
             loader=PackageLoader(__name__),
         )
 
-    def requestStarted(self, job):
-        url = job.requestUrl()
-        request = url.authority()
-
-        if request not in PAGES:
-            path = url.path()
-            if path.startswith("/js/"):
-                js_path = os.path.join(THIS_DIR, "js", path[4:])
-                if os.path.isfile(js_path):
-                    f = QFile(js_path, self)
-                    job.reply(b"application/javascript", f)
-
-            return
-
-        template = self.env.get_template(request + ".html")
-
-        fn = PAGES[request]
-
+    def reply_template(self, job, tpl_name, data):
+        template = self.env.get_template(tpl_name + ".html")
         buffer = QBuffer(self)
-        buffer.setData(template.render(**fn(self)).encode("utf-8"))
+        buffer.setData(template.render(**data).encode("utf-8"))
         job.reply(b"text/html", buffer)
 
-    @register_page
-    def version(self):
-        return {
+    def requestStarted(self, job):
+        url = job.requestUrl()
+        url_s = url.toString()[10:]  # strip webmacs://
+
+        for re_match, meth in _REQUESTS_HANDLER:
+            res = re_match.match(url_s)
+            if res:
+                meth(self, job, url, *res.groups())
+                return
+
+    @register_page(match_url=r"^/js/(.+\.js)$", visible=False)
+    def to_js(self, job, _, path):
+        js_path = os.path.join(THIS_DIR, "js", path)
+        if os.path.isfile(js_path):
+            f = QFile(js_path, self)
+            job.reply(b"application/javascript", f)
+
+    @register_page()
+    def version(self, job, _, name):
+        self.reply_template(job, name, {
             "versions": (
                 ("Webmacs version", version.WEBMACS_VERSION_STR),
                 ("Qt version", version.QT_VERSION_STR),
                 ("Chromium version", version.chromium_version()),
                 ("Opengl vendor", version.opengl_vendor()),
             )
-        }
+        })
 
-    @register_page
-    def downloads(self):
-        return {}
+    @register_page()
+    def downloads(self, job, _, name):
+        self.reply_template(job, name, {})
+
+    @register_page()
+    def commands(self, job, _, name):
+        self.reply_template(job, name, {"commands": COMMANDS})
+
+    @register_page()
+    def variables(self, job, _, name):
+        self.reply_template(job, name, {"variables": VARIABLES})
