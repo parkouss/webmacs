1b5f3081992ffa3c1d2495cb809c25e86a933813
Julien Pagès <j.parkouss@gmail.com>
2018-09-01 05:27:49 +0200

    Add Python version information in webmacs://version

--- webmacs/scheme_handlers/webmacs/__init__.py
+++ webmacs/scheme_handlers/webmacs/__init__.py
@@ -14,6 +14,7 @@
 # along with webmacs.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import sys
 import re
 import importlib
 import inspect
@@ -85,6 +86,7 @@ class WebmacsSchemeHandler(QWebEngineUrlSchemeHandler):
         self.reply_template(job, name, {
             "versions": (
                 ("Webmacs version", version.WEBMACS_VERSION_STR),
+                ("Python version", sys.version),
                 ("Qt version", version.QT_VERSION_STR),
                 ("Chromium version", version.chromium_version()),
                 ("Opengl vendor", version.opengl_vendor()),
