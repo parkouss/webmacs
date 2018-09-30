39a3dee34b98fbf5b4b7895852f149bf14d04c9f
Julien Pagès <j.parkouss@gmail.com>
2018-09-30 08:01:22 +0200

    fix iframe navigation tests
    Now that they block inside the command execution.

--- tests/integration/test_iframe_navigation.py
+++ tests/integration/test_iframe_navigation.py
@@ -48,20 +48,34 @@ def test_iframe_follow(session, pytestconfig, hint_method, variables):
     variables.set("hint-method", hint_method)
     session.load_page("iframe_follow", wait_iframes=True)
 
-    with session.wait_hints_ready():
-        session.keyclick("f")
+    end_waiter = session.waiter()
 
-    if hint_method == "filter":
-        session.wkeyclicks("C-n")
-        # wait until the background color is green, the above keypress has been
-        # taken in account.
-        session.check_nav_highlighted(INPUT0_IFRAME)
-        session.wkeyclicks("Enter")
-    else:
-        session.keyclick("d")
+    def do_check():
+        session.check_javascript(
+            "%s === window.frames[0].document.activeElement"
+            % INPUT0_IFRAME, True)
+        session.keyclicks("youhou")
 
-    session.check_javascript("%s === window.frames[0].document.activeElement"
-                             % INPUT0_IFRAME, True)
-    session.keyclicks("youhou")
+        session.check_javascript("%s.value" % INPUT0_IFRAME, "youhou")
+        end_waiter.set()
 
-    session.check_javascript("%s.value" % INPUT0_IFRAME, "youhou")
+    def prompt_follow(prompt, _):
+        wait_hint.wait()
+        if hint_method == "filter":
+            session.wkeyclicks("C-n")
+            # wait until the background color is green, the above keypress has
+            # been taken in account.
+            session.check_nav_highlighted(INPUT0_IFRAME)
+
+            with session.wait_signal(prompt.closed):
+                session.wkeyclicks("Enter")
+        else:
+            with session.wait_signal(prompt.closed):
+                session.keyclick("d")
+
+        session.call_next(do_check)
+
+    session.set_prompt_exec(prompt_follow)
+    wait_hint = session.wait_hints_ready()
+    session.wkeyclicks("f")
+    end_waiter.wait()
